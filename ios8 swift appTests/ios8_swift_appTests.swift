0b38df1a7f92678acfd2c3187fc73a26bc2fa253
吉亨斗 <kilh@yoshitooruto-no-MacBook-Air.local>
2014-06-23 11:03:41 +0900

    Initial Commit

---
+++ ios8 swift appTests/ios8_swift_appTests.swift
@@ -0,0 +1,35 @@
+//
+//  ios8_swift_appTests.swift
+//  ios8 swift appTests
+//
+//  Created by 吉亨斗 on 2014/06/23.
+//  Copyright (c) 2014年 吉亨斗. All rights reserved.
+//
+
+import XCTest
+
+class ios8_swift_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
